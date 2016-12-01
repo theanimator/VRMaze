3c0d011857af96edaece33714442c638beaa2cc8
Laura Wieme <laurawieme@gmail.com>
2016-12-01 11:32:32 -0800

    Upload screenshots and scripts

---
+++ CoinScore.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CoinScore : MonoBehaviour {
+
+	private int score;
+	Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		scoreText = GetComponent<Text>();
+		scoreText.text="Coins: " + score + "/7";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void IncreaseScore ()
+	{
+		score++;
+		UpdateScore ();
+	}
+
+	void UpdateScore ()
+	{
+		scoreText.text = "Coins: " + score + "/7";
+	}
+}
